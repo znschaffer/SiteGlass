41cd42cb3ab58b35891125fb73afb5a75b86a437
Zane Schaffer <znschaffer@gmail.com>
2025-04-04 14:05:08 -0700

    show current rating of site on click

--- background.js
+++ background.js
@@ -1,7 +1,13 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "getData") {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            sendResponse(tabs[0].url);
+        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+            domain = new URL(tabs[0].url);
+
+            const params = new URLSearchParams({query: domain.hostname});
+            fetch(`https://api.tosdr.org/search/v5?${params}`).then((response) => response.json()).then((data) => {
+                sendResponse(data);
+            });
+
         })
     }
     return true;
--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
 chrome.runtime.sendMessage({ from: "popup", action: "getData" }, (response) => {
-    document.getElementById("tabName").innerHTML = response;
+    //
+    document.getElementById("tabName").innerHTML = response.services[0].rating;
+    console.log(response);
 });
