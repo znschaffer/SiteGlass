e7aa3e64cfb002ea5e5976cdcc55a463145a3248
JD Faber <savannah.5@LIVE.COM>
2025-04-04 14:58:14 -0700

    Tried to mimic grade search and have broken both parts :( not sure how to fix

--- popup.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.sendMessage({ from: "popup", action: "getData" }, (response) => {
-    //
-    document.getElementById("tabName").innerHTML = response.services[0].rating;
-    console.log(response);
-});
