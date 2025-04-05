d097a2d6f8c6abff56ea0549ef59e17b9885e461
JD Faber <savannah.5@LIVE.COM>
2025-04-04 18:49:41 -0700

    Now it has line breaks and conditional works!

--- popup.js
+++ popup.js
@@ -5,12 +5,12 @@ chrome.runtime.sendMessage({ from: "popup", action: "getData" }, (response) => {
 
 chrome.runtime.sendMessage({ from: "popup", action: "getBreaches" }, (response) => {
     document.getElementById("hostBreaches").innerHTML = Object.keys(response).length; // the Amount of Responses
-    // Check if There Are Any Breaches Before Printing
+    // If Breaches > 0, Get Breach Data
     if (Object.keys(response).length > 0) {
-        document.getElementById("hostRecentBreach").innerHTML = response[0].BreachDate; // the Most Recent Breach Date
-        document.getElementById("hostRecentBreachSummary").innerHTML = response[0].Description; // the Recent Breach Summary
-        //document.getElementById("hostName").innerHTML = response[0].Name; // the Host Name
-        document.getElementById("hostBreachCount").innerHTML = response[0].PwnCount; // the Amount of People Affected
+        let breachStats;
+        breachStats = "The most recent breach was on: " + response[0].BreachDate + "<br><br>A summary of this breach is: " + response[0].Description + "<br><br>The amount of people affected by this breach is: " + response[0].PwnCount;
+
+        document.getElementById("breachStats").innerHTML = breachStats;
     }
     console.log(response);
 });
