1efaf80b9bb4d4779b2f9ee283992baceff5c0f0
JD Faber <savannah.5@LIVE.COM>
2025-04-04 23:42:39 -0700

    Working in Breaches branch for coding w/ breach information- will toss in main

--- popup.js
+++ popup.js
@@ -5,17 +5,41 @@ chrome.runtime.sendMessage({ from: "popup", action: "getData" }, (response) => {
 
 chrome.runtime.sendMessage({ from: "popup", action: "getBreaches" }, (response) => {
     document.getElementById("hostBreaches").innerHTML = Object.keys(response).length; // the Amount of Breaches
-    // If Breaches > 0, Get Breach Data
-    if (Object.keys(response).length > 0) {
         let totalPwnCount = 0;
         // Calculate Total Affected People
         for (let i = 0; i < Object.keys(response).length; i++) {
             totalPwnCount += response[i].PwnCount;
         }
-
-        let breachStats;
-        breachStats = "The most recent breach was on: " + response[0].BreachDate + "<br><br>A summary of this breach is: " + response[0].Description + "<br><br>The amount of people affected by all of the breaches is: " + totalPwnCount;
-        document.getElementById("breachStats").innerHTML = breachStats;
-    }
+        document.getElementById("breachStats").innerHTML = totalPwnCount;
+        document.getElementById("hostLastBreach").innerHTML = response[0].BreachDate;
+        document.getElementById("hostName").innerHTML = response[0].Domain;
     console.log(response);
 });
+
+document.getElementById("clearCache").addEventListener("click", () => {
+    chrome.runtime.sendMessage({from: "popup", action: "clearCache"}, (response) => {
+        document.getElementById("clearCache").innerHTML = "Cache Cleared";
+        document.getElementById("clearCache").disabled = "none";
+    })
+})
+
+
+document.querySelectorAll('.tabButton').forEach(button => {
+    button.addEventListener('click', () => {
+        // Remove active class from all buttons
+        document.querySelectorAll('.tabButton').forEach(btn => btn.classList.remove('tabButton--active'));
+
+        // Add active class to the clicked button
+        button.classList.add('tabButton--active');
+
+        // Hide all tab contents
+        document.querySelectorAll('.tab').forEach(tab => tab.style.display = 'none');
+
+        // Show the corresponding tab content
+        const tabId = button.getAttribute('data-tab');
+        document.getElementById(tabId).style.display = 'flex';
+    });
+});
+
+// Initially show the first tab
+document.querySelector('.tabButton--active').click();
