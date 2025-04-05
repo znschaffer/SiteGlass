d6eac54acfdf687a84fe93d708f1b477859a7cee
Zane Schaffer <101823296+znschaffer@users.noreply.github.com>
2025-04-04 20:32:02 -0700

    add ability to clear local api cache (#10)

--- background.js
+++ background.js
@@ -1,55 +1,65 @@
+function clearCache(request, sendResponse) {
+    chrome.storage.local.clear().then(_ => sendResponse({message: "Cache cleared."}));
+    return true;
+}
 
 function getSiteRating(request, sendResponse) {
-    if (request.action === "getData") {
-        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-            const domain = new URL(tabs[0].url).hostname;
-            const params = new URLSearchParams({query: domain});
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        let domain = new URL(tabs[0].url).hostname
 
-            chrome.storage.local.get(domain, (result) => {
-                const cached = result[domain];
+        // handle leading www
+        if (domain.split('.')[0] === 'www') {
+            domain = domain.slice(4)
+        }
 
-                if (!cached) {
-                    let url = `https://api.tosdr.org/search/v5?${params}`
-                    console.log(url)
-                    // Fetch from API if not cached
-                    fetch(url)
-                        .then((response) => response.json())
-                        .then((resp) => {
-                            let data = null
-                            console.log(resp)
-                            console.log(params)
-                            resp.services.find((item) => {
-                                if (item.urls.includes(domain)) {
-                                    data = item
-                                }
-                                console.log(item)
-                            })
-                            console.log(data)
-                            if (data) {
-                                // Cache the data
-                                chrome.storage.local.set({[domain]: data}, () => {
-                                    sendResponse(data);
-                                });
-                            } else {
-                                sendResponse(null);
+        const params = new URLSearchParams({query: domain});
+
+        chrome.storage.local.get(domain, (result) => {
+            const cached = result[domain];
+            if (!cached) {
+                let url = `https://api.tosdr.org/search/v5?${params}`
+                console.log(url)
+                // Fetch from API if not cached
+                fetch(url)
+                    .then((response) => response.json())
+                    .then((resp) => {
+                        let data = null
+                        resp.services.find((item) => {
+                            if (item.urls.includes(domain)) {
+                                data = item
                             }
-
                         })
-                        .catch((error) => {
-                            console.error("Fetch error:", error);
-                            sendResponse({error: "Failed to fetch data"});
-                        });
-                } else {
-                    // Serve from cache
-                    sendResponse(cached);
-                }
-            });
-        });
 
-        return true; // Keeps the message channel open for sendResponse
-    }
+                        if (data) {
+                            // Cache the data
+                            chrome.storage.local.set({[domain]: data}, () => {
+                                sendResponse(data);
+                            });
+                        } else {
+                            sendResponse(null);
+                        }
+
+                    })
+                    .catch((error) => {
+                        console.error("Fetch error:", error);
+                        sendResponse({error: "Failed to fetch data"});
+                    });
+            } else {
+                // Serve from cache
+                sendResponse(cached);
+            }
+        });
+    });
+
+    return true; // Keeps the message channel open for sendResponse
+
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    return getSiteRating(request, sendResponse);
+    if (request.action === "getSiteRating") {
+        return getSiteRating(request, sendResponse);
+    } else if (request.action === "clearCache") {
+        return clearCache(request, sendResponse);
+    }
+
 });
--- popup.js
+++ popup.js
@@ -1,4 +1,11 @@
-chrome.runtime.sendMessage({from: "popup", action: "getData"}, (response) => {
-    document.getElementById("tabName").innerHTML = response.rating
-    console.log(response);
+chrome.runtime.sendMessage({from: "popup", action: "getSiteRating"}, (response) => {
+    document.getElementById("rating").innerHTML = response.rating
+    document.getElementById("siteName").innerHTML = response.urls[0]
 });
+
+document.getElementById("clearCache").addEventListener("click", () => {
+    chrome.runtime.sendMessage({from: "popup", action: "clearCache"}, (response) => {
+        document.getElementById("clearCache").innerHTML = "Cache Cleared";
+        document.getElementById("clearCache").disabled = "none";
+    })
+})
