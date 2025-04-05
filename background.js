edbb0a3fb05216b5d418e3e0063a55784631e9b6
Austin-AA <austin.aragon@wsu.edu>
2025-04-05 03:38:06 -0700

    Cookie Branch Pull (#20)
    * implemented getCookies
    
    Cookies are retrieved from the users tab and its data is spit out to the extension
    
    * Total Count working
    
    * Total counts and counts per bucket working
    
    TODO:
    Create new functions to find cookie types better
    
    Redo listeners so we are not doing the same work over and over again.
    
    * First party cookies implemented
    
    * final cookies commit
    
    fixed/cleaned things up. misc should now be calculated, if even used
    
    ---------
    
    Co-authored-by: Zane Schaffer <znschaffer@gmail.com>

--- background.js
+++ background.js
@@ -93,14 +93,108 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
         })
         return true;
+
     }
 
+    if (request.action === "getCookies") {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            chrome.cookies.getAll({ url: tabs[0].url }).then((cookies) => {
+
+                sendResponse({ success: true,length: cookies.size, cookies });
+            }).catch((error) => {
+                console.error(error);
+                sendResponse({ success: false, error: "Failed to retrieve cookies." });
+            });
+        });
+        return true;
+    }
+
+        if (request.action === "getCookiesCount") {
+            chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+                chrome.cookies.getAll({ url: tabs[0].url }).then((cookies) => {
+
+                    sendResponse({ success: true,length: cookies.size, cookies});
+                }).catch((error) => {
+                    console.error(error);
+                    sendResponse({ success: false, error: "Failed to retrieve cookies." });
+                });
+            });
+            return true;
+        }
+
+        if (request.action === "functionalCount") {
+            chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+                chrome.cookies.getAll({ url: tabs[0].url }).then((cookies) => {
+
+                    sendResponse({ success: true,length: cookies.size, cookies});
+                }).catch((error) => {
+                    console.error(error);
+                    sendResponse({ success: false, error: "Failed to retrieve cookies." });
+                });
+            });
+            return true;
+        }
+
+        if (request.action === "analyticalCount") {
+            chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+                chrome.cookies.getAll({ url: tabs[0].url }).then((cookies) => {
+
+                    sendResponse({ success: true,length: cookies.size, cookies});
+                }).catch((error) => {
+                    console.error(error);
+                    sendResponse({ success: false, error: "Failed to retrieve cookies." });
+                });
+            });
+            return true;
+        }
+
+        if (request.action === "marketingCount") {
+            chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+                chrome.cookies.getAll({ url: tabs[0].url }).then((cookies) => {
+
+                    sendResponse({ success: true,length: cookies.size, cookies});
+                }).catch((error) => {
+                    console.error(error);
+                    sendResponse({ success: false, error: "Failed to retrieve cookies." });
+                });
+            });
+            return true;
+        }
+
+        if (request.action === "miscCount") {
+            chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+                chrome.cookies.getAll({ url: tabs[0].url }).then((cookies) => {
+
+                    sendResponse({ success: true,length: cookies.size, cookies});
+                }).catch((error) => {
+                    console.error(error);
+                    sendResponse({ success: false, error: "Failed to retrieve cookies." });
+                });
+            });
+            return true;
+        }
+
+        if (request.action === "firstPartyCookies") {
+            chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            chrome.cookies.getAll({ url: tabs[0].url }).then((cookies) => {
+              url = new URL(tabs[0].url);
+              domain = url.hostname;
+
+              sendResponse({ success: true, domain, cookies});
+
+                }).catch((error) => {
+                    console.error(error);
+                    sendResponse({ success: false, error: "Failed to retrieve cookies." });
+                });
+            });
+            return true;
+        }
 });
 
 // Helper Method for Splitting Domain
 function SplitDomain(url) {
     const urlConst = new URL(url);
-    const splitLink = urlConst.hostname.split('.'); // Splits link @ . 
+    const splitLink = urlConst.hostname.split('.'); // Splits link @ .
 
     let domainLink = splitLink.slice(-2);
 
--- popup.js
+++ popup.js
@@ -64,3 +64,146 @@ document.querySelectorAll('.tabButton').forEach(button => {
 // Initially show the first tab
 document.querySelector('.tabButton--active').click();
 
+
+document.addEventListener("DOMContentLoaded", () => {
+    chrome.runtime.sendMessage({ from: "popup", action: "getData" }, (response) => {
+        //
+        document.getElementById("tabName").innerHTML = response.services[0].rating;
+        console.log(response);
+    });
+
+    chrome.runtime.sendMessage({ from: "popup", action: "getCookies" }, (response) => {
+        if (chrome.runtime.lastError) {
+            console.error("Error:", chrome.runtime.lastError.message);
+            document.getElementById("getCookies").textContent = "Failed to retrieve cookies.";
+            return;
+        }
+
+        if (response && response.cookies) {
+            const cookiesList = response.cookies.map(cookie => `${cookie.name}: ${cookie.value}: ${cookie.domain}`);
+            document.getElementById("getCookies").textContent = cookiesList.join(", ");
+            //document.getElementById("getCookiesCount").textContent = response.cookiesList.size;
+
+
+
+        } else {
+            document.getElementById("getCookies").textContent = "No cookies found.";
+        }
+        console.log("GetCookies Response:", response);
+    });
+
+    chrome.runtime.sendMessage({ from: "popup", action: "getCookiesCount" }, (response) => {
+            if (chrome.runtime.lastError) {
+                console.error("Error:", chrome.runtime.lastError.message);
+                document.getElementById("getCookiesCount").textContent = "Failed to retrieve cookies.";
+                return;
+            }
+
+            if (response && response.cookies) {
+                const cookiesList = response.cookies.map(cookie => `${cookie.name}: ${cookie.value}`);
+                document.getElementById("getCookiesCount").textContent = response.cookies.length;
+
+            } else {
+                document.getElementById("getCookiesCount").textContent = "No cookies found.";
+            }
+            console.log("GetCookies Count Response:", response);
+        });
+
+
+    chrome.runtime.sendMessage({ from: "popup", action: "functionalCount" }, (response) => {
+        if (response && response.cookies) {
+
+            const result = organizeCookies(response.cookies, "Functional");
+            document.getElementById("functionalCount").textContent = result.length;
+
+        } else {
+                console.error("No cookies received.");
+            }
+        });
+
+
+    chrome.runtime.sendMessage({ from: "popup", action: "analyticalCount" }, (response) => {
+        if (response && response.cookies) {
+
+            result = organizeCookies(response.cookies, "Analysis");
+            document.getElementById("analyticalCount").textContent = result.length;
+
+        } else {
+                console.error("No cookies received.");
+            }
+
+    });
+
+    chrome.runtime.sendMessage({ from: "popup", action: "marketingCount" }, (response) => {
+        if (response && response.cookies) {
+
+            result = organizeCookies(response.cookies, "Marketing");
+            document.getElementById("marketingCount").textContent = result.length;
+
+
+        } else {
+                console.error("No cookies received.");
+            }
+
+    });
+    chrome.runtime.sendMessage({ from: "popup", action: "miscCount" }, (response) => {
+        if (response && response.cookies) {
+
+            document.getElementById("miscCount").textContent = 0; //temp value - misc should be calculated based of others results
+
+        } else {
+                console.error("No cookies received.");
+            }
+    });
+
+    chrome.runtime.sendMessage({ from: "popup", action: "firstPartyCookies" }, (response) => {
+
+        if (response && response.cookies && response.domain) {
+            firstPartyArray = []
+            response.cookies.forEach(cookie => {
+
+                if(response.domain.includes(cookie.domain) || cookie.domain.includes(response.domain))
+                {
+                    firstPartyArray.push(cookie);
+                }
+            });
+            document.getElementById("firstPartyCookies").textContent = firstPartyArray.length;
+
+
+        } else {
+                console.error("No cookies received.");
+            }
+    });
+
+});
+
+
+function organizeCookies(cookies, category) {
+    const bucket = []
+
+    cookies.forEach(cookie => {
+
+    switch (category) {
+    case "Functional":
+        if (cookie.name.includes("session") || cookie.name.includes("auth") || cookie.name.includes("language") || cookie.name.includes("tz")) {
+            bucket.push(cookie);
+        }
+        break;
+    case "Marketing":
+        if (cookie.name.includes("ad") || cookie.name.includes("campaign") || cookie.name.includes("sid")) {
+            bucket.push(cookie);
+        }
+        break;
+    case "Analysis":
+
+        if (cookie.name.includes("analytics") || cookie.name.includes("track")) {
+            bucket.push(cookie);
+        }
+        break;
+    default:
+    }
+
+    });
+
+    return bucket;
+}
