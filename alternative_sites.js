2829289459e0fe319c266cf42e4566aed1406faa
hippotipper <hippotipper@gmail.com>
2025-04-05 05:35:16 -0700

    new alternative sites and alt ui

--- alternative_sites.js
+++ alternative_sites.js
@@ -1,13 +1,37 @@
 const altSiteCache = {
-    "google" : "Brave Search: Ranking at a B for privacy and using it's own index Brave search may be a better choice for a search engine.",
-    "youtube" : "FreeTube ranking at an A for privacy gives users much more control and security as well as allows them to avoid ads. The development team is much smaller so some features are lacking, but if privacy is most  important this is a great alternative."
+    "amazon": {"search": "Amazon", "alternative": "EcoCart", "link": "https://ecocart.io/"},
+    "bing" : {"search": "Bing", "alternative": "Brave", "link" : "https://brave.com/"},
+    "dropbox": {"search": "Dropbox", "alternative": "OnionShare", "link": "https://onionshare.org/#download"},
+    "facebook": {"search": "Facebook", "alternative": "Mastodon", "link": "https://mastodon.social/explore"},
+    "instagram": {"search": "Instagram", "alternative": "Mastodon", "link": "https://mastodon.social/explore"},
+    "reddit": {"search": "Reddit", "alternative": "Lemmy", "link": "https://join-lemmy.org/"},
+    "google" : {"search": "Google", "alternative": "Brave", "link" : "https://brave.com/"},
+    "googlemaps": {"search": "Google Maps", "alternative": "OpenStreetMap", "link": "https://www.openstreetmap.org/"},
+    "youtube" : {"search": "YouTube", "alternative": "FreeTube", "link": "https://freetubeapp.io/#download"},
+    "vscode": {"search": "VSCode", "alternative": "VSCodium", "link": "https://vscodium.com/#install"},
+    "x" : {"search": "X", "alternative": "Bluesky", "link": "https://bsky.app/"},
+    "tiktok" : {"search": "TikTok", "alternative": "Bluesky", "link": "https://bsky.app/"},
+    "yahoo": {"search": "Yahoo", "alternative": "ProtonMail", "link": "https://proton.me/mail"},
+    "live": {"search": "Live", "alternative": "ProtonMail", "link": "https://proton.me/mail"},
+    "taboola": {"search": "Taboola", "alternative": "Minds", "link": "https://www.minds.com/"},
+    "foxnews": {"search": "Fox News", "alternative": "Minds", "link": "https://www.minds.com/"},
+    "yandex" : {"search": "Yandex", "alternative": "Brave", "link" : "https://brave.com/"},
+    "twitch" : {"search": "Twitch", "alternative": "PeerTube", "link": "https://joinpeertube.org/"},
+    "office": {"search": "Office", "alternative": "Libre Office", "link": "https://www.libreoffice.org/"},
+    "aliexpress": {"search": "Aliexpress", "alternative": "EcoCart", "link": "https://ecocart.io/"},
+    "walmart": {"search": "Walmart", "alternative": "EcoCart", "link": "https://ecocart.io/"},
+    "temu": {"search": "Temu", "alternative": "EcoCart", "link": "https://ecocart.io/"},
+    "stackoverflow": {"search": "Stack Overflow", "alternative": "Dev.to", "link": "https://dev.to/"},
+    "none" : {"search": "", "alternative": "There are currently no recommended alternatives for this site.", "link": "none"}
 };
 
 function getSiteAlternatives(searchSite){
-    let site = searchSite.split(".")
-    site = site[0]
-    site = site.toLowerCase()
+    let site = searchSite.toLowerCase()
     if(site in altSiteCache){
         return (altSiteCache[site]);
+    } else {
+        let noResult = altSiteCache["none"]
+        noResult.search = searchSite
+        return noResult
     }
 }
