30cc06abe75d396615fc727ce87b4980da7b3f4a
JD Faber <savannah.5@LIVE.COM>
2025-04-04 20:57:48 -0700

    Turns out you just have to route it to a JavaScript file

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
 chrome.runtime.sendMessage({from: "popup", action: "getData"}, (response) => {
-    document.getElementById("tabName").innerHTML = response.rating
-    console.log(response);
+    //document.getElementById("tabName").innerHTML = response.rating
+    //console.log(response);
 });
