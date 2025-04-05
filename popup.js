5b2329d5991ce0ef098d2795f47f28cbdbed8f26
JD Faber <savannah.5@LIVE.COM>
2025-04-04 18:16:28 -0700

    scratch that- the issue was it was running into an unused getElementById before PwnCount

--- popup.js
+++ popup.js
@@ -9,7 +9,7 @@ chrome.runtime.sendMessage({ from: "popup", action: "getBreaches" }, (response) 
     if (Object.keys(response).length > 0) {
         document.getElementById("hostRecentBreach").innerHTML = response[0].BreachDate; // the Most Recent Breach Date
         document.getElementById("hostRecentBreachSummary").innerHTML = response[0].Description; // the Recent Breach Summary
-        document.getElementById("hostName").innerHTML = response[0].Name; // the Host Name
+        //document.getElementById("hostName").innerHTML = response[0].Name; // the Host Name
         document.getElementById("hostBreachCount").innerHTML = response[0].PwnCount; // the Amount of People Affected
     }
     console.log(response);
