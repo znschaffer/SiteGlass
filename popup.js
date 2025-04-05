7db7eacff89fffffdbbf562660749ed781e04909
JD Faber <savannah.5@LIVE.COM>
2025-04-04 18:59:33 -0700

    Gets total PwnCount rather than just most recent

--- popup.js
+++ popup.js
@@ -4,12 +4,17 @@ chrome.runtime.sendMessage({ from: "popup", action: "getData" }, (response) => {
 });
 
 chrome.runtime.sendMessage({ from: "popup", action: "getBreaches" }, (response) => {
-    document.getElementById("hostBreaches").innerHTML = Object.keys(response).length; // the Amount of Responses
+    document.getElementById("hostBreaches").innerHTML = Object.keys(response).length; // the Amount of Breaches
     // If Breaches > 0, Get Breach Data
     if (Object.keys(response).length > 0) {
-        let breachStats;
-        breachStats = "The most recent breach was on: " + response[0].BreachDate + "<br><br>A summary of this breach is: " + response[0].Description + "<br><br>The amount of people affected by this breach is: " + response[0].PwnCount;
+        let totalPwnCount = 0;
+        // Calculate Total Affected People
+        for (let i = 0; i < Object.keys(response).length; i++) {
+            totalPwnCount += response[i].PwnCount;
+        }
 
+        let breachStats;
+        breachStats = "The most recent breach was on: " + response[0].BreachDate + "<br><br>A summary of this breach is: " + response[0].Description + "<br><br>The amount of people affected by all of the breaches is: " + totalPwnCount;
         document.getElementById("breachStats").innerHTML = breachStats;
     }
     console.log(response);
