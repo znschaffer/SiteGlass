66c570188251ce34b1cf460fc53e9d5579093ebe
JD Faber <savannah.5@LIVE.COM>
2025-04-04 18:03:32 -0700

    Working on getting PwnCount - I think it may need to be converted to string or something

--- popup.js
+++ popup.js
@@ -4,6 +4,13 @@ chrome.runtime.sendMessage({ from: "popup", action: "getData" }, (response) => {
 });
 
 chrome.runtime.sendMessage({ from: "popup", action: "getBreaches" }, (response) => {
-    document.getElementById("hostName").innerHTML = Object.keys(response).length; // Return the Amount of Responses
+    document.getElementById("hostBreaches").innerHTML = Object.keys(response).length; // the Amount of Responses
+    // Check if There Are Any Breaches Before Printing
+    if (Object.keys(response).length > 0) {
+        document.getElementById("hostRecentBreach").innerHTML = response[0].BreachDate; // the Most Recent Breach Date
+        document.getElementById("hostRecentBreachSummary").innerHTML = response[0].Description; // the Recent Breach Summary
+        document.getElementById("hostName").innerHTML = response[0].Name; // the Host Name
+        document.getElementById("hostBreachCount").innerHTML = response[0].PwnCount; // the Amount of People Affected
+    }
     console.log(response);
 });
